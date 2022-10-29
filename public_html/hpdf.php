510db7f85e8e9c25586bdf3e0edb7f6fd161ffb3
Juan Teran <sebasterra69@gmail.com>
2022-10-29 12:12:41 -0500

    login
    crear img pra el pdf y archivo txt

--- public_html/hpdf.php
+++ public_html/hpdf.php
@@ -5,9 +5,9 @@
     $doc=new FPDF();
     $doc->AddPage('L','A4');
    
-    $doc->Image('images/fia.png',10,10,50,28,'PNG');
+    $doc->Image('images/c++',10,10,50,28,'PNG');
     $doc->SetFont('Helvetica','B','30');
-    $doc->Text('60','30','Federecion Internacional de Automovilismo');
+    $doc->Text('60','30','Nombre empresa');
     $doc->SetFont('Arial','B','20');
     $doc->Text('135','40','Otorga');
     $doc->SetFont('Arial','I','30');
@@ -16,14 +16,14 @@
     $doc->Text('140','70','a:');
     $doc->Ln();
     $doc->SetFont('Courier','I','35');
-    $doc->Text('50','90',$_POST['nom']);
+    $doc->Text('50','90',$_POST['']);
     $doc->SetFont('Arial','','20');
-    $doc->Text('40','120','Por haber finalizado el dia '.$_POST['fecha'].' el curso de '.$_POST['curso']);
+    $doc->Text('40','120','Por haber finalizado el dia '.$_POST['fecha'].' el curso de C++');
     $doc->SetFont('Courier','I','20');
     $doc->Text('100','130','Con duracion de '.$_POST['hrs'].' horas.');
     $doc->Image('images/mmf.png',20,157,35,15,'PNG');
-    $doc->Text('20','175',$_POST['prof']);
-    $doc->Text('20','180',$_POST['ocu']);
+    $doc->Text('20','175','Nombre prof');
+    
     $doc->Output('constancia.pdf','I');
 
 ?>
